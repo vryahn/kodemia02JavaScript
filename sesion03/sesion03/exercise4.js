a02511f0ffa9833b17c27ed3596a5f38e9995616
Bryan Rodriguez <b.ra@live.com.mx>
2022-09-28 21:48:55 -0500

    Class exercises and homework

---
+++ sesion03/sesion03/exercise4.js
@@ -0,0 +1,11 @@
+// Ejercicio 4. Muestra los primeros 100 números enteros iniciando desde el 1
+
+const enteros = () => {
+    let contador = 1;
+    while(contador<=100){
+        console.log(contador);
+        contador++
+    }
+}
+
+enteros();
