ea54470ecb0b733478ef039bb861cea98a00c232
Bryan Rodríguez <b.ra@live.commxm>
2022-09-30 14:14:21 -0500

    lining up with prettier

--- sesion05-arrays/homeWork10.js
+++ sesion05-arrays/homeWork10.js
@@ -1,21 +1,20 @@
 // homeWork10. Escribir un programa que pida al usuario un número entero y muestre por pantalla un triángulo rectándulo como el siguiente:
 /*
-*
-**
-***
-****
-*****
-*/
+ *
+ **
+ ***
+ ****
+ *****
+ */
 
 let num = Number(prompt("Ingrese numero aqui mero: "));
-let a= "*";
-let b= "*";
+let a = "*";
+let b = "*";
 const triangle = (s) => {
-    for(let i=1;i<=s;i++)
-    {        
-      console.log(a);
-      a=a+b;
-    }
-}
+  for (let i = 1; i <= s; i++) {
+    console.log(a);
+    a = a + b;
+  }
+};
 
 triangle(num);
