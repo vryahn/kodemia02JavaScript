3f5666d16b14ee98d91695b90b8abec8ab3e4d87
Bryan Rodríguez <b.ra@live.commxm>
2022-09-30 09:51:42 -0500

    homeWork2 done

---
+++ sesion05-arrays/homeWork2.js
@@ -0,0 +1,20 @@
+// homeWork2. El usuario ingresará sucesivamente un número, el programa deberá dejar de solicitar un valor si el número ingresado está en el rango de 1 a 5.
+
+let num = [];
+
+const getNumbers = () => {
+  let i = 0;
+  let getOut = false;
+  do {
+    num[i] = Number(prompt("Introduzca número " + (i + 1) + ": "));
+    if(num[i]>=1&&num[i]<=5) getOut=true;
+    i++;
+  } while(getOut==false);
+};
+
+const printNumbers = () => {
+    for(let i=0;i<num.length;i++) console.log(num[i]);
+}
+
+getNumbers();
+printNumbers();
