3c168e61dda19f98fca35e8d60efb71c08f8aa0a
Bryan Rodríguez <b.ra@live.commxm>
2022-09-30 09:19:50 -0500

    homeWork1. done

--- sesion05-arrays/exercise3-arrays.js
+++ sesion05-arrays/exercise3-arrays.js
@@ -1,3 +1,10 @@
 // Exercise 3. Crear una función que pueda contar cuantas veces aparece un elemento dado en los parámetros en un arreglo también dado en sus parámetros
 
 // 1. Aislar elementos únicos en un nuevo arreglo. Posteriormente revisar cuantas veces se repite cada uno de los valores del nuevo arreglo, en el arreglo original.
+
+let array = [1,2,2,3,4,4,4,4,5,5,6,6,6,6,10,10];
+
+const isolating = () => {
+    arrayClone = array;
+    
+}
---
+++ sesion05-arrays/homeWork1.js
@@ -0,0 +1,25 @@
+// homeWork1. Introducir un conjunto de calificaciones (no se conoce el número de calificaciones). La entrada termina al introducir un valor negativo. Al final se deberá entregar el número de calificaciones que entraron y el promedio general.
+
+let califications = [];
+let average = 0;
+let adittion = 0;
+
+const getCalifications = () => {
+  let i = 0;
+  let getOut = false;
+  
+  do {
+    califications[i] = Number(prompt("Introduzca Calificacion " + (i + 1) + ": "));
+    if (califications[i] > 0) {
+      adittion = adittion + califications[i];
+    } else getOut=true; //Al detectar un número negativo, descarta éste último en la suma de calificaciones y cambia la condición getOut para salir del bucle.
+    i++;
+  } while(getOut==false);
+  
+};
+
+getCalifications();
+average = adittion / (califications.length-1);
+console.log("Suma: "+adittion);
+console.log("Total de Calificaciones Ingresadas: "+(califications.length-1));
+console.log("Promedio General: "+average);
