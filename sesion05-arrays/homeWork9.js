172fa9c7dc210bfc2769a59ec5a0ad465e1ee934
Bryan Rodríguez <b.ra@live.commxm>
2022-09-30 12:46:33 -0500

    homeWork9 done

---
+++ sesion05-arrays/homeWork9.js
@@ -0,0 +1,11 @@
+// homeWork9. Dado un número entero mostrar la lista de los números pares inferiores a él.
+
+let num = prompt('Indique primer número: ');
+let i=num-1;
+
+while(i>0)
+{
+    if(i%2==0)console.log(i);
+    i--;
+    if(i==0)break;
+}
