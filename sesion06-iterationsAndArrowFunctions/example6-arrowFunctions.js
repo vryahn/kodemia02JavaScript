bb939efc46517e9b89efea23ae5b54c3b947e66b
Bryan Rodriguez <b.ra@live.com.mx>
2022-10-04 11:35:44 -0500

    example6-arrowFunctions

---
+++ sesion06-iterationsAndArrowFunctions/example6-arrowFunctions.js
@@ -0,0 +1,5 @@
+// Ejemplo 6. Funciones Flecha. Return Implícito. Las funciones flecha tienen la ventaja de retornar un valor sin necesidad de usar la palabra reservada return.
+
+const sumaDosNumeros = (num1,num2) => num1+num2;
+
+sumaDosNumeros(1,3);
