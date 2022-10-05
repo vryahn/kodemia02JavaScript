2a4455837e6e99745d5cd0053c7701600fdc8a3b
Bryan Rodriguez <b.ra@live.com.mx>
2022-10-05 10:28:36 -0500

    homeWork2. alpha

--- sesion06-iterationsAndArrowFunctions/homeWork2-arrowFunctions.js
+++ sesion06-iterationsAndArrowFunctions/homeWork2-arrowFunctions.js
@@ -1,5 +1,9 @@
 /* homeWork2. Arrow Functions.
-Crear una función que recibe un string con varias palabras y acambia a mayúscula la primera letra de cada palabra.
+Crear una función que recibe un string con varias palabras y cambia a mayúscula la primera letra de cada palabra.
 */
+
+let frase = 'hola mundo';
+frase = frase.toUpperCase;
+console.log(frase);
 
 // ::: WORK PENDING :::
