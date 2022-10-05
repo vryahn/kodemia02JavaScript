34e5f33e30e089b951e7e82951c935ae6938d884
Bryan Rodriguez <b.ra@live.com.mx>
2022-10-05 11:04:34 -0500

    specialHomework done

--- sesion06-iterationsAndArrowFunctions/specialHomeWork.js
+++ sesion06-iterationsAndArrowFunctions/specialHomeWork.js
@@ -4,4 +4,27 @@ Dado un arreglo de puntuaciones debes ordenarlo de menor a mayor, imprimir el re
 Pista: Quicksort.
 */
 
-// ::: WORK PENDING :::
+let myArr = [85, 24, 63, 45, 17, 31, 96, 50];
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  } // Si la longitud de la matriz es menor que 1, devuelve la matriz directamente
+  let left = []; // La matriz de la izquierda
+  let right = []; // La matriz de la derecha
+  let pivotIndex = Math.floor(arr.length / 2); // Seleccione el punto de referencia, el método floor () es redondeado hacia abajo
+  let pivot = arr.splice(pivotIndex, 1); // Matriz base
+  for (let i = 0; i < arr.length; i++) {
+    // Luego comience a recorrer la matriz, los elementos más pequeños que "base" se colocan en el subconjunto izquierdo y los elementos más grandes que la base se colocan en el subconjunto derecho.
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right)); // Usa la recursión para repetir este proceso continuamente, puedes obtener la matriz ordenada.
+};
+
+let a = quickSort(myArr);
+console.log('La puntuación mas baja fue: '+a[0]);
+
+// WARNING: ESTE CÓDIGO ME LO FUSILÉ DE INTERNET, AUN LO ESTOY ESTUDIANDO
