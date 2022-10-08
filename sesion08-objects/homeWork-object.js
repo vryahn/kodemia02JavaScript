0c5819087f2adc2bb8e4f262647a8e9dc9f3439f
Bryan Rodriguez <b.ra@live.com.mx>
2022-10-07 19:22:36 -0500

    homeWork beta3

--- sesion08-objects/homeWork-object.js
+++ sesion08-objects/homeWork-object.js
@@ -1,12 +1,70 @@
 // homeWork 1. En una empresa que se dedica a vender productos por internet, necesita un pequeño programa para registrar las ventas del día, por cada producto se tiene un SKU, precio, descripción, modelo , número de serie. Esta última característica puede o no tenerla, sin embargo, todas las propiedades anteriores (Excepto número de serie ) son obligatorias. Una vez registradas todas las ventas del día, deberás hacer el cálculo de todo lo vendido en el día, notificándoselo al usuario, así como el producto más vendido del día, en este caso el producto se debió de vender al menos más de dos veces.
 
-const createProduct = (sku,precio,descripcion,modelo,serie='') => ({
+const createProduct = (sku, precio, descripcion, version = "", serie = "") => ({
   sku,
   precio,
   descripcion,
-  modelo,
-  serie
-})
+  version,
+  serie,
+});
+
+ejemplos = createProduct();
+
+ejemplos = [
+  {
+    sku: "K001",
+    precio: 11000,
+    descripción: "PlayStation 5",
+  },
+  {
+    sku: "K002",
+    precio: 6000,
+    descripción: "Xbox Series S",
+  },
+  {
+    sku: "K003",
+    precio: 14000,
+    descripción: "Xbox Series X",
+  },
+  {
+    sku: "K004",
+    precio: 6000,
+    descripción: "Nintendo Switch",
+  },
+  {
+    sku: "K005",
+    precio: 8000,
+    descripción: "Nintendo Switch OLED",
+  },
+];
+
+const ingresandoVentas = () => {
+  console.log("Bienvenido al ingreso de Ventas");
+  choice = console.confirm("Desea ingresar un elemento vendido?");
+  if (choice == false) {
+    console.log("Entonces que chingados hace usted aqui? ... buen dia :)");
+  } else {
+    do {
+      registrarVenta();
+      choice = console.confirm("Desea ingresar un elemento adicional?");
+      if(choice==false)
+      {
+        devolverSuma();
+        devolverMasVendido();
+        console.log('Chau Gor2')
+      }
+    } while (choice == true);
+  }
+};
+
+const registrarVenta = () => {
+  let x = null;
+  switch (x){
+    
+  }
+}
+
+/* ::::: Version con Prompt :::::
 
 let choice = null;
 let products = [];
@@ -33,85 +91,4 @@ const totalSells = (products) => {
 
 console.log('El total vendido fue de : '+totalSells(products));
 
-// Aqui se viene lo bueno...
-
-// Primero en arrayIsoled guardamos los elementos de array, sin repetirse.
-
-let arrayIsoled = [];
-let arrayCount = [];
-
-const countingRepeated = () => {
-
-let indexIsoled = 0;
-for (let i = 0; i < products.length; i++)
-  {
-    let saving = true;
-    for (let x = 0; x < products.length; x++)
-      {
-        if (i != x)
-        {
-        	if (products[i].sku == arrayIsoled[x]) saving=false;
-      	}
-      } if(saving==true)
-      	{
-        	arrayIsoled[indexIsoled]=products[i].sku;
-          indexIsoled++;
-      	}
-  }
-
-// En arrayCount guardaremos la cantidad de repeticiones por elemento, primero lo inicializamos en 0 con un tamaño de elementos igual a arrayIsoled
-  
-for (let i = 0; i < arrayIsoled.length; i++)
-  {
-    arrayCount[i]=0;
-  }
-
-// Ahora veremos para cada elemento de arrayIsoled, cuantas veces aparece en el arreglo original, y guardaremos cada aparicion del elemento en arrayCount
-  
-for (let i = 0; i < arrayIsoled.length; i++)
-  {
-    for (let x = 0; x < products.length; x++)
-      {
-        	if (arrayIsoled[i] == products[x].sku) arrayCount[i]++;
-      }
-  }
-}
-
-// Por último, imprimimos el resultado de forma amigable al usuario
-
-const printResult = () => {
-  for (let i = 0; i < arrayIsoled.length; i++)
-    {
-      if(arrayCount[i]==1)
-        {
-          console.log("El Numero "+arrayIsoled[i]+" se repite: "+arrayCount[i]+" vez")
-        } else {
-          console.log("El Numero "+arrayIsoled[i]+" se repite: "+arrayCount[i]+" veces")
-        }
-    }
-}
-
-let mayor = arrayCount[0];
-console.log(mayor);
-let mayorIndex;
-  for(let i=1;i<=arrayCount.length;i++)
-    {
-      if(mayor<arrayCount[i]) 
-      {
-        mayor = arrayCount[i];
-        mayorIndex = i;
-      }
-    }
-console.log(mayor);
-
-let bestSeller;
-for(key in products){
-  if(arrayIsoled[mayorIndex]==products[key].sku) {
-    bestSeller = products[key].descripcion;
-    break;
-  }
-}
-
-
-countingRepeated();
-console.log('El producto más vendido se vendio: '+mayor+' veces y fue: '+bestSeller);
+*/
