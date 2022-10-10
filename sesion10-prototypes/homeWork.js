5295203e0aca7e14d07f1d7b715b786ccd1440cf
Bryan Rodriguez <b.ra@live.com.mx>
2022-10-09 21:43:23 -0500

    section c done

--- sesion10-prototypes/homeWork.js
+++ sesion10-prototypes/homeWork.js
@@ -2,40 +2,39 @@
 Dado un arreglo de nombres de archivo con su extensión, imprime en consola la extensión del archivo, el nombre del archivo en minúsculas, así como conocer si contienen la cadena "kodemia" en el nombre.
 */
 
-const files = ['Kodemia.txt','temario.docx','main.js','index.html'];
+const files = ["Kodemia.txt", "temario.docx", "main.js", "index.html"];
 
 // Funcion a). Imprime en consola la extensión del archivo.
 
 const extensions = (archivos) => {
   let nameFiles = [];
-  for(let i=0;i<archivos.length;i++)
-    {    nameFiles[i] = archivos[i].substr((archivos[i].indexOf('.')+1))
-     console.log(nameFiles[i])
-    }
-}
+  for (let i = 0; i < archivos.length; i++) {
+    nameFiles[i] = archivos[i].substr(archivos[i].indexOf(".") + 1);
+    console.log(nameFiles[i]);
+  }
+};
 extensions(files);
 
 // Funcion b). Imprime en consola el nombre del archivo en minúsculas.
 
 const lowerNames = (archivos) => {
   let nameFiles = [];
-  for(let i=0;i<archivos.length;i++)
-    {    nameFiles[i] = archivos[i].substr(0,(archivos[i].indexOf('.')))
-     nameFiles[i] = nameFiles[i].toLowerCase();
-     console.log(nameFiles[i])
-    }
-}
+  for (let i = 0; i < archivos.length; i++) {
+    nameFiles[i] = archivos[i].substr(0, archivos[i].indexOf("."));
+    nameFiles[i] = nameFiles[i].toLowerCase();
+    console.log(nameFiles[i]);
+  }
+};
 lowerNames(files);
 
 // Funcion c). Conocer si contienen la cadena "kodemia" en el nombre.
-/*
-const lookForKodemia = (names) => {
-    for(name in names) {
-    if(name.search('kodemia')==0) return true;
-   } return false;
-}
-lookForKodemia(files);
-*/
-let x = false;
-if(files[0].search('endo')==0) x=true;
-x;
+
+const loofForKodemia = (archivos) => {
+    for(archivo of archivos){
+    if(archivo.search('kodemia')>=0 || archivo.search('Kodemia')>=0)
+      {
+        console.log('Encontrado')
+      }
+    }
+  };
+  loofForKodemia(files);
