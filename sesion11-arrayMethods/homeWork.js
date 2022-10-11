edd6a8c91a246a8c23c70359887693641a308bc5
Bryan Rodriguez <b.ra@live.com.mx>
2022-10-11 06:57:14 -0500

    homework instructions

---
+++ sesion11-arrayMethods/homeWork.js
@@ -0,0 +1,4 @@
+/* homeWork. Métodos de arreglos
+Dado un arreglo de puntuaciones debes ordenarlo de menor a mayor, imprimir el resultado en consola, posteriormente deberás remover y recuperar la última puntuación, mostrarás en consola “La puntuación x es la más baja”, debes mostrar nuevamente el arreglo y por último convierte ese arreglo en una cadena de caracteres y mostrarlo en consola.
+*/
+
