cffabb8f530e2c48d81ff35ad39421493ad77c00
Bryan Rodriguez <b.ra@live.com.mx>
2022-10-25 23:34:16 -0500

    complete class

--- sesion22-eventsagain/main.js
+++ sesion22-eventsagain/main.js
@@ -1,18 +1,36 @@
-const div = document.getElementById('test-event');
+const title = document.getElementById("test-event");
+const loginForm = document.querySelector("#login-form");
 
-div.addEventListener('click', (event) => {
-    event.preventDefault();
-    event.target;
-    console.log('Ejecutaste el evento', event);
-    
+title.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  event.target;
+  console.log("Ejectuaste el evento ", event);
 });
 
-/* 
-function añadirEvento (eventType, callback){
-    // Registra el evento
-    callback(evento);
-};
+/// Rcuperar los valores de los inputs
 
-añadirEvento('click',(argumento1) => {
-    console.log(argumento1);
-}) */
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#login-form__email").value;
+  const password = document.querySelector("#login-form__password").value;
+  const rememberMe = document.querySelector("#login-form__rememberme").value;
+
+  console.log(email, password, rememberMe);
+
+  //   const myInput = document.querySelectorAll("input");
+  //   const arrayInput = Array.from(myInput).map((input) => {
+  //     return input.value;
+  //     // console.log(input.value);
+  //   });
+  //   console.log("Este es el arrayInput", arrayInput);
+
+  const data = Array.from(event.target.children)
+    .filter((element) => {
+      if (element.tagName == "INPUT") return true;
+      return false;
+    })
+    .map((element) => element.value);
+  console.log(data);
+});
--- sesion22-eventsagain/notes.md
+++ sesion22-eventsagain/notes.md
@@ -14,3 +14,11 @@ Ocurre cuando se presiona la tecla/botón y este llega arriba nuevamente
 ## Formularios
 
 La acción por default del tipo "reset" es limpiar los input que se tengan dentro del mismo form.
+
+### Tipos de input
+
+submit
+
+### Métodos para envío de datos
+
+Por medio del GET nosotros enviamos los datos por medio de la URL
