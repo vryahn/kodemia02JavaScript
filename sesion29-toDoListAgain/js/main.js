57cb064bab573dcb03fc60ded46ef25f7b15e6f8
Bryan Rodriguez <b.ra@live.com.mx>
2022-11-11 22:15:31 -0600

    sending tasks

--- sesion29-toDoListAgain/js/main.js
+++ sesion29-toDoListAgain/js/main.js
@@ -1,4 +1,32 @@
-const db = 'https://hey-arnold-default-rtdb.firebaseio.com/to-do-list';
+const db = "https://hey-arnold-default-rtdb.firebaseio.com/to-do-list";
 
 // Crear tarea
-const newTaskDescriptionInput = document.getElementById("new-task_description");
+const newTaskDescriptionInput = document.querySelector("#new-task_description");
+
+const newTaskBtn = document.querySelector("#new-task_button");
+
+newTaskBtn.addEventListener("click", () => {
+  const newTask = {
+    description: newTaskDescriptionInput.value,
+    isCompleted: "false",
+  };
+  fetch(db + ".json", {
+    method: "POST",
+    body: JSON.stringify(newTask),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    return res.json()
+  }).then((data) => {
+    const.log("Recibí estos datos :", data);
+  }).catch((err)=> {
+    console.error("Ocurrió un error: ", err);
+  });
+
+  console.log("Nueva tarea", newTask);
+});
+
+/* .then((res) => res.json())
+.then((response) => console.log("Success", response))
+.catch((error) => console.error("Error: ", error)); */
